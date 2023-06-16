297c940437f64e60639da45d4498b233e1aad2c9
zthanos <zthanoshtc@hotmail.com>
2023-06-16 16:28:17 +0300

    working somehow

--- lib/line_tokenizer.ex
+++ lib/line_tokenizer.ex
@@ -2,24 +2,28 @@ defmodule LineTokenizer do
 
   def parse_line(line) do
     tokens = apply_tokenization_rules(line, tokenization_rules())
-    {:paragraph, tokens}
+    tokens
   end
 
+
   defp apply_tokenization_rules(line, []) do
-    [line]
+    [%{attribute: :normal, value: line}]
   end
 
   defp apply_tokenization_rules(line, [regex | rest]) do
     case Regex.run(regex.rule, line) do
       [_token | [_, val, _]] ->
-        [Atom.to_string(regex.attr) | apply_tokenization_rules(val, rest)]
+        %{attribute: Atom.to_string(regex.attr), value: apply_tokenization_rules(val, rest)}
       _ ->
         apply_tokenization_rules(line, rest)
     end
   end
 
+
+
   defp tokenization_rules do
     [
+      %{rule: ~r/(\*{2})(.*?)(\1)/, attr: :normal},       # Bold
       %{rule: ~r/(\*{2})(.*?)(\1)/, attr: :bold},       # Bold
       %{rule: ~r/(_{2})(.*?)(\1)/, attr: :Bold},        # Bold
       %{rule: ~r/(_{1})(.*?)(\1)/, attr: :Bold},        # Italic
